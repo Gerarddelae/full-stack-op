01daa830d1d64999b327078a56f56f4d0d25c0eb
Gerardo <gerardmoe@hotmail.com>
2023-06-30 17:32:44 -0500

    Exercise 1.3

--- part1/src/index.js
+++ part1/src/index.js
@@ -21,20 +21,26 @@ const Total = ({exercises}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} numberExercises={exercises1} />
-      <Content part={part2} numberExercises={exercises2} />
-      <Content part={part3} numberExercises={exercises3} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Content part={part1.name} numberExercises={part1.exercises} />
+      <Content part={part2.name} numberExercises={part2.exercises} />
+      <Content part={part3.name} numberExercises={part3.exercises} />
+      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   )
 }
