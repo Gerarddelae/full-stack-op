7e79bd20c4b481ff243d0941803816f41ea2805b
Gerardo <gerardmoe@hotmail.com>
2023-07-15 16:34:15 -0500

    Mudanza sistema

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -52,7 +52,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Phonebook App</h2>
       <Filter handleFilter={handleFilter} />
       <h3>Add a new</h3>
       <PersonForm
